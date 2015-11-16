8d6e5132eeebe78bc09fac9b90b17e464df74fd2
alexander <duanhjlt@gmail.com>
2015-11-16 23:59:34 +0800

    add test code.

--- StackViewDemo/ViewController.swift
+++ StackViewDemo/ViewController.swift
@@ -26,5 +26,24 @@ class ViewController: UIViewController
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func removeStar(sender: AnyObject) {
+        let star: UIView? = self.horizontalStackView.arrangedSubviews.last
+        if let aStar = star {
+            self.horizontalStackView.removeArrangedSubview(aStar)
+            aStar .removeFromSuperview()
+            UIView.animateWithDuration(0.25, animations: { () -> Void in
+                self.horizontalStackView.layoutIfNeeded()
+            })
+        }
+    }
+    
+    @IBAction func addStar(sender: AnyObject) {
+        let starImageView: UIImageView = UIImageView(image: UIImage(named: "star"))
+        starImageView.contentMode = .ScaleAspectFit
+        self.horizontalStackView.addArrangedSubview(starImageView)
+        UIView.animateWithDuration(0.25) { () -> Void in
+            self.horizontalStackView.layoutIfNeeded()
+        }
+    }
 }
 
